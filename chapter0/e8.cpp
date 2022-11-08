e8aaa978331a96ce151ea0a9c7440116b37c145f
raymondmills <millsraymondj@gmail.com>
2022-11-08 13:42:22 -0500

    exercise 0-8

---
+++ chapter0/e8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// This program is valid -- the */ does not close the // opening delimiter
+int main()
+{
+    // This is a comment that extends over several lines
+    // by using // at the beginning of each line instead of using /*
+    // or */ to delimit comments
+
+    std::cout << "Does this work?" << std::endl;
+
+    return 0;
+}
