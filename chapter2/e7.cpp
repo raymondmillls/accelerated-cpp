c863b5af5ff4cda2d17bfc7bd7f644b062d45dfb
raymondmillls <millsraymondj@gmail.com>
2022-12-05 17:34:08 -0500

    completed exercise 7

---
+++ chapter2/e7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int i = 10;
+    while (i > -6) {
+        cout << i << endl;
+        i -= 1;
+    }
+
+    return 0;
+}
