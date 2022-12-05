59905d81ed72090b06cd8f3f2a7e78b272f35e62
raymondmillls <millsraymondj@gmail.com>
2022-12-05 17:32:02 -0500

    excercise 6

---
+++ chapter2/e6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+// This program prints numbers 1 through 10
+int main()
+{
+    int i = 0;
+    while (i < 10) {
+        i += 1;
+        cout << i << endl;
+    }
+
+    return 0;
+}
